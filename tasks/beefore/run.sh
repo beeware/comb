c2baf3cb774a905b211fd626788a9d1572b6dab8
Russell Keith-Magee <russell@keith-magee.com>
2018-01-26 14:52:44 +1100

    Modifications to support local beekeeper builds.

--- tasks/beefore/run.sh
+++ tasks/beefore/run.sh
@@ -5,11 +5,20 @@ echo "Node version=`node --version`"
 echo "NPM version=`npm --version`"
 echo
 # Download and unpack code at the test SHA
-echo "curl -L $CODE_URL -o code.zip"
-curl -s -L $CODE_URL -o code.zip
 echo "--------------------------------------------------------------------------------"
-echo "unzip code.zip"
-unzip code.zip
+if [ "$CODE_URL" ]; then
+    echo "Testing $GITHUB_OWNER/$GITHUB_PROJECT_NAME @ $SHA"
+    echo
+    # Download and unpack code at the test SHA
+    echo "curl -L $CODE_URL -o code.zip"
+    curl -s -L $CODE_URL -o code.zip
+    echo "--------------------------------------------------------------------------------"
+    echo "unzip code.zip"
+    unzip code.zip
+    mv $GITHUB_PROJECT_NAME-$SHA src
+else
+    echo "Using local code checkout."
+fi
 echo "--------------------------------------------------------------------------------"
 echo pip install beefore
 pip install beefore
@@ -20,10 +29,10 @@ echo "    --username $GITHUB_USERNAME"
 echo "    --repository $GITHUB_OWNER/$GITHUB_PROJECT_NAME"
 echo "    --pull-request $GITHUB_PR_NUMBER"
 echo "    --commit $SHA"
-echo "    $TASK $GITHUB_PROJECT_NAME-$SHA"
+echo "    $TASK src"
 
 beefore --username $GITHUB_USERNAME \
     --repository $GITHUB_OWNER/$GITHUB_PROJECT_NAME \
     --pull-request $GITHUB_PR_NUMBER \
     --commit $SHA \
-    $TASK $GITHUB_PROJECT_NAME-$SHA
+    $TASK src
--- tasks/py34/run.sh
+++ tasks/py34/run.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
-echo "Testing $GITHUB_OWNER/$GITHUB_PROJECT_NAME @ $SHA"
 echo "Python version=`python --version`"
-echo
-# Download and unpack code at the test SHA
-echo "curl -L $CODE_URL -o code.zip"
-curl -s -L $CODE_URL -o code.zip
+if [ "$CODE_URL" ]; then
+    echo "Testing $GITHUB_OWNER/$GITHUB_PROJECT_NAME @ $SHA"
+    echo
+    # Download and unpack code at the test SHA
+    echo "curl -L $CODE_URL -o code.zip"
+    curl -s -L $CODE_URL -o code.zip
+    echo "--------------------------------------------------------------------------------"
+    echo "unzip code.zip"
+    unzip code.zip
+    mv $GITHUB_PROJECT_NAME-$SHA src
+else
+    echo "Using local code checkout."
+fi
 echo "--------------------------------------------------------------------------------"
-echo "unzip code.zip"
-unzip code.zip
-
+if [ "$EXTRA_REQUIRES" ]; then
+    for pkg in $EXTRA_REQUIRES; do
+        echo Installing extra requirement $pkg
+        pip install $pkg
+    done
+else
+    echo "No extra requirements"
+fi
 echo "--------------------------------------------------------------------------------"
-for pkg in $EXTRA_REQUIRES; do
-    echo Installing extra requirement $pkg
-    pip install $pkg
-done
-echo "--------------------------------------------------------------------------------"
-cd $GITHUB_PROJECT_NAME-$SHA
-for pkg_dir in $SRC_REQUIRES; do
-    echo Installing requirement $pkg_dir from src
-    pushd $pkg_dir
-    pip install -e .
-    popd
-done
+cd src
+if [ "$SRC_REQUIRES" ]; then
+    for pkg_dir in $SRC_REQUIRES; do
+        echo Installing requirement $pkg_dir from src
+        pushd $pkg_dir
+        pip install -e .
+        popd
+    done
+else
+    echo "No source requirements"
+fi
 echo "--------------------------------------------------------------------------------"
 if [ "$TEST_DIR" ]; then
     pushd $TEST_DIR
-    echo Installing test code from $TEST_DIR
+    echo "Installing test code from $TEST_DIR"
 fi
 echo pip install -e .
 pip install -e .
@@ -33,16 +45,20 @@ if [ "$TEST_DIR" ]; then
     popd
 fi
 echo "--------------------------------------------------------------------------------"
-for pkg_dir in $SRC_DEPENDANTS; do
-    echo Installing dependant $pkg_dir from src
-    pushd $pkg_dir
-    pip install -e .
-    popd
-done
+if [ "$SRC_DEPENDANTS" ]; then
+    for pkg_dir in $SRC_DEPENDANTS; do
+        echo "Installing dependant $pkg_dir from src"
+        pushd $pkg_dir
+        pip install -e .
+        popd
+    done
+else
+    echo "No source dependants"
+fi
 echo "================================================================================"
 if [ "$TEST_DIR" ]; then
     cd $TEST_DIR
-    echo Running test code from $TEST_DIR
+    echo "Running test code from $TEST_DIR"
 fi
 echo python setup.py test
 python setup.py test
