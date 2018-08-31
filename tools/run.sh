613318e2090dd3902a884a9a5da3f982286e1709
Russell Keith-Magee <russell@keith-magee.com>
2018-08-31 09:34:32 +0800

    Clean up the output of java version.

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Python version=`python --version`"
-echo "Java version=`java -version`"
+java -version
 echo "Beefore version=`beefore --version`"
 echo
 if [ -e "/app/beekeeper.yml" ]; then
